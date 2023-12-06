523362e2b8b3f64324621b09374b482e2c1992de
CaldatoLuca <lucacaldato15@gmail.com>
2023-12-06 16:39:12 +0100

    feat: bonus jumbo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,28 @@
+<script>
+import AppHeroBlock from "./AppHeroBlock.vue";
+export default {
+  name: "Jumbo app",
+  data() {
+    return {
+      author: "Luca Caldato",
+    };
+  },
+  components: {
+    AppHeroBlock,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <AppHeroBlock />
+  </div>
+  <h3>By {{ author }}</h3>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  border: 1px solid white;
+}
+</style>
---
+++ src/components/AppHeroBlock.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "Jumbo block",
+};
+</script>
+
+<template>
+  <div class="jumbo-block"><h2>npm</h2></div>
+  <div class="jumbo-block"><h2>vue</h2></div>
+  <div class="jumbo-block"><h2>vite</h2></div>
+</template>
+
+<style scoped>
+.jumbo-block {
+  border: 1px solid white;
+  margin: 20px;
+  width: calc(100% / 3 - 40px);
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+}
+</style>
